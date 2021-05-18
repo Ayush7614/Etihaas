17dad9754fff5a2cad7836aa7cce8a66304c918c
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2021-05-18 12:23:27 +0530

    Create facts.js

---
+++ js/facts.js
@@ -0,0 +1,55 @@
+/*
+Author - Ayush Kumar
+Etihaas
+CHROME EXTENSION
+*/
+var quote=""; 
+$(document).ready(function(){
+        var d=new Date();
+        $('#quoteDisplay').html('Hang On! We\'re searching for you.');
+         $("#quoteDisplay").append("<br><i class=\"fas fa-spinner fa-spin loader\"></i>");
+        $.ajax({
+	url: 'http://numbersapi.com/'+(d.getMonth()+1)+'/'+d.getDate()+'/date',
+	type: 'GET',
+	success: function(data){
+		console.log(data);
+        quote=data;
+        $('#quoteDisplay').html(data);
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
+});
+
+function newFact()
+    {
+        $('#quoteDisplay').html('Hang On! We\'re searching for you.');
+         $("#quoteDisplay").append("<br><i class=\"fas fa-spinner fa-spin loader\"></i>");
+        $.ajax({
+	url: 'http://numbersapi.com/10/19/date',
+	type: 'GET',
+	success: function(data){
+		console.log(data);
+        quote=data;
+        $('#quoteDisplay').html(data);
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
+    }
+
+//function to tweet
+
+$("#tweet").click(function(e){
+         e.preventDefault();
+         window.location = 'https://twitter.com/intent/tweet?url=https://github.com/vinitshahdeo/&text='+quote+'&hashtags=TodayInHistory,MadeByAyushKumar';    
+});
+
+// function to share on whatsapp
+
+$("#whatsapp").click(function(e){
+         e.preventDefault();
+         window.location = 'https://wa.me/?text='+quote+' -- {{Shared via Chrome Extension made by Ayush Kumar}}';    
+});
